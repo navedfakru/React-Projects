7fe84d8d860b112fda1a6a1d2f55794f457d5bd7
Naved Ali <naved10055@gamil.com>
2024-06-25 23:13:29 +0530

    reactrouter

---
+++ 07reactRouter/src/components/index.js
@@ -0,0 +1,7 @@
+import About from "./About/About";
+import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import User from "./User/User";
+export {Home, Footer, Header, About, Contact, User}

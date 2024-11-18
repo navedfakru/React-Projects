b357f8a3cadf47fc953df9e455d6ceafea143ce9
Naved Ali <naved10055@gmail.com>
2024-11-18 14:53:26 +0530

    form in reactjs wow projects

--- Nike/src/sections/CustomerRevies.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CustomerRevies = () => {
-  return (
-    <div>CustomerRevies</div>
-  )
-}
-
-export default CustomerRevies
--- Nike/src/sections/PopularProduct.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const PopularProduct = () => {
-  return (
-    <div>PopularProduct</div>
-  )
-}
-
-export default PopularProduct

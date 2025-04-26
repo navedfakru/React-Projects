a29dd32049a576103a851e5857fd5a2050c912ce
Naved <naved10055@gmail.com>
2025-04-26 10:16:15 +0530

    auth snippent for appwrite

--- reactdocs/src/App.jsx
+++ reactdocs/src/App.jsx
@@ -1,19 +1,13 @@
 import { useState } from 'react'
 import Button from './components/Button'
 import './App.css'
+import Component from './components/Component'
 
 function App() {
 
   return (
     <>
-      <div className='toolbar' onClick={() => alert("you click inside div")}>
-        <Button onClick={() => alert("Playing!")}>
-          Playing
-        </Button>
-        <Button onClick={() => alert("Upload image")}>
-          Upload image
-        </Button>
-      </div>
+      <Component />
     </>
   )
 }
